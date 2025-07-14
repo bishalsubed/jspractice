660f5c87a1bdd2fb57053c102999ce6b9d727175
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-07-14 17:20:31 +0545

    little improvement

--- index.js
+++ index.js
@@ -1,79 +1,92 @@
-// class InMemoryCache {
-//     constructor(capacity) {
-//         // capacity limit
-//         // store key-node map
-//         // track head/tail of LRU linked list
-//         // maybe a timer or interval for TTL cleanup
-//     }
-
-//     get(key) {
-//         // check if key exists
-//         // check TTL
-//         // update LRU order
-//         // return value or null
-//     }
-
-//     set(key, value, ttl) {
-//         // if key exists, update value and TTL
-//         // else if at capacity, evict LRU
-//         // add new key with TTL
-//         // move to head of usage list
-//     }
-
-//     delete(key) {
-//         // optional: remove a specific key
-//     }
-
-//     clear() {
-//         // optional: reset the whole cache
-//     }
-
-//     // internal: moveToHead(node), removeNode(node), etc.
-// }
-
-
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
+    constructor(key, value, ttl) {
+        this.key = key;
+        this.value = value;
+        this.ttl = ttl
+        this.next = null;
+        this.prev = null;
     }
 }
-class LinkedList {
-    constructor() {
-        this.head = null
+class InMemoryCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.head = null;
+        this.tail = null;
+
+        // capacity limit
+        // store key-node map
+        // track head/tail of LRU linked list
+        // maybe a timer or interval for TTL cleanup
     }
-    append(value) {
-        let newnode = new Node(value)
-        console.log("Newnode: ",newnode)
+
+    get(key) {
+        // check if key exists
+        // check TTL
+        // update LRU order
+        // return value or null
+    }
+
+    set(key, value, ttl) {
+        const map = new Map();
         if (!this.head) {
-            this.head = newnode
-            console.log("Head: ",this.head)
+            let newValue = new Node(key, value, ttl)
+            map.set(key, value)
+            this.head = newValue;
+            this.tail = newValue;
             return
         }
-        let current = this.head
-        console.log("Current: ",current)
-        while (current.next) {
-            current = current.next
-            console.log("Inside current: ",current)
+        let tillNow = this.head
+        if (tillNow.key === key) {
+            tillNow.value = value;
+            tillNow.ttl = ttl;
         }
-        current.next = newnode
-        console.log("Current after append: ",current)
-        console.log("Head ",this.head)
+        while (tillNow.next) {
+            if (tillNow.key === key) {
+                tillNow.value = value;
+                tillNow.ttl = ttl;
+            }
+            tillNow = tillNow.next;
+        }
+        if(map.size >= this.capacity){
+            this.removeLastNode()
+        }
+        let newValue = new Node(key, value, ttl)
+        map.set(key, value)
+        newValue.next = this.head;
+        this.head.prev = newValue
+        this.head = newValue;
+
+        // if key exists, update value and TTL
+        // else if at capacity, evict LRU
+        // add new key with TTL
+        // move to head of usage list
     }
+
+    delete(key) {
+        // optional: remove a specific key
+    }
+
+    clear() {
+        // optional: reset the whole cache
+    }
+
     printList() {
         let current = this.head
-        console.log("Printing List: ", current)
         let result = ""
-        console.log("Result: ", result)
         while (current) {
             result += current.value + '->'
             current = current.next
         }
         console.log(result + 'null')
     }
+
+    removeLastNode() {
+        if (!this.tail) return;
+        if (this.tail.prev) {
+            this.tail.prev.next = null;
+            this.tail = this.tail.rev
+        }
+    }
+
+    // internal: moveToHead(node), removeNode(node), etc.
 }
-let list = new LinkedList()
-list.append(10)
-list.append(20)
-list.append(30)
-list.printList()
