37cea3d3f24136920e3a0c4ac64fe836b87808f8
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-07-14 19:17:45 +0545

    delete a paticular key added

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ class InMemoryCache {
         this.capacity = capacity;
         this.head = null;
         this.tail = null;
+        this.map = new Map();
 
         // capacity limit
         // store key-node map
         // track head/tail of LRU linked list
         // maybe a timer or interval for TTL cleanup
     }
-
     get(key) {
         // check if key exists
         // check TTL
@@ -27,10 +27,9 @@ class InMemoryCache {
     }
 
     set(key, value, ttl) {
-        const map = new Map();
         if (!this.head) {
             let newValue = new Node(key, value, ttl)
-            map.set(key, value)
+            this.map.set(key, value)
             this.head = newValue;
             this.tail = newValue;
             return
@@ -41,20 +40,24 @@ class InMemoryCache {
             tillNow.ttl = ttl;
         }
         while (tillNow.next) {
+            tillNow = tillNow.next;
             if (tillNow.key === key) {
                 tillNow.value = value;
                 tillNow.ttl = ttl;
+                this.head.prev = tillNow;
+                this.head = tillNow;
+                this.head.prev = null;
             }
-            tillNow = tillNow.next;
         }
-        if(map.size >= this.capacity){
+        if (this.map.size >= this.capacity) {
             this.removeLastNode()
         }
         let newValue = new Node(key, value, ttl)
-        map.set(key, value)
+        this.map.set(key, value)
         newValue.next = this.head;
         this.head.prev = newValue
         this.head = newValue;
+
 
         // if key exists, update value and TTL
         // else if at capacity, evict LRU
@@ -63,7 +66,29 @@ class InMemoryCache {
     }
 
     delete(key) {
-        // optional: remove a specific key
+        if (!this.map.has(key)) return;
+        let current = this.head
+        if (current.key === key) {
+            if(!current.next){
+                current.key = null;
+                current.value = null;
+                current.ttl = null;
+                return
+            }
+            current.next.prev = null;
+            this.head = current.next
+            this.map.delete(key);
+            return;
+        }
+        while (current.next) {
+            current = current.next
+            if (current.key === key) {
+                current.prev.next = null
+                current = current.prev
+                return
+            }
+        }
+        this.map.delete(key);
     }
 
     clear() {
@@ -84,9 +109,19 @@ class InMemoryCache {
         if (!this.tail) return;
         if (this.tail.prev) {
             this.tail.prev.next = null;
-            this.tail = this.tail.rev
+            this.map.delete(this.tail.key)
+            this.tail = this.tail.prev
         }
     }
 
     // internal: moveToHead(node), removeNode(node), etc.
 }
+
+const cache = new InMemoryCache(3);
+cache.set('a', 1, 1000);
+cache.set('b', 2, 1000);
+cache.set('c', 3, 1000);
+cache.delete('a')
+cache.delete('c')
+cache.delete('b')
+cache.printList();
